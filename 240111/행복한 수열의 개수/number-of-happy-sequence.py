af33c92dcda17c276670c518bcbd284cb8551640
Moonto <45365194+Moonto@users.noreply.github.com>
2024-01-11 15:35:25 +0900

    Title: 행복한 수열의 개수, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240111/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,39 @@
+n,m=map(int,input().split())
+
+arr=[]
+
+for i in range(n):
+    arr.append(list(map(int,input().split())))
+
+row_max=0
+temp_row_max=0
+col_max=0
+temp_col_max=0
+
+mCnt=0
+
+for col in range(n-1):
+    for row in range(n-1):
+        if arr[row][col] == arr[row+1][col]:
+            temp_row_max+=1
+            row_max=temp_row_max
+            continue
+        else:
+            temp_row_max=0
+            continue
+    if row_max>=m:
+        mCnt+=1
+
+for row in range(n-1):
+    for col in range(n-1):
+        if arr[row][col] == arr[row][col+1]:
+            temp_col_max+=1
+            col_max=temp_col_max
+            continue
+        else:
+            temp_col_max=0
+            continue
+    if col_max>=m:
+        mCnt+=1
+
+print(mCnt)
